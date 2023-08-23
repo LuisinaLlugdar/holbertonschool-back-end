c0b7d9ccf0ab686d68a0bdbd0b7ca020ffa488bb
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-08-23 14:30:54 -0700

    task0 made

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Script that uses a given REST API. For a given employee ID, returns
+information about his/her TODO list progress.
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    TASK_TITLE = []
+
+    try:
+        emp_id = int(argv[1])
+    except Exception:
+        exit()
+
+    emp_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{emp_id}")
+    todo_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{emp_id}/todos")
+
+    for task in todo_response.json():
+        if task.get("completed") is True:
+            NUMBER_OF_DONE_TASKS += 1
+            TOTAL_NUMBER_OF_TASKS += 1
+            TASK_TITLE.append(task.get("title"))
+        else:
+            TOTAL_NUMBER_OF_TASKS += 1
+
+    EMPLOYEE_NAME = emp_response.json()["name"]
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    for task in TASK_TITLE:
+        print("\t {}".format(task))
