28fa5fa02eab94ab826e5fc325fb45cbce626097
LuisinaLlugdar <luisina.llugdar2@gmail.com>
2023-08-23 14:53:25 -0700

    task1 made

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script that uses a given REST API and exports data in the CSV format
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    try:
+        emp_id = int(argv[1])
+    except Exception:
+        exit()
+
+    emp_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{emp_id}").json()
+    todo_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{emp_id}/todos").json()
+
+    name = emp_response['username']
+    file_name = f"{emp_id}.csv"
+    with open(file_name, "w", encoding="utf-8") as file:
+        for task in todo_response:
+            task_completed = task.get('completed')
+            task_title = task.get('title')
+            file.write(
+                f'"{emp_id}","{name}","{task_completed}","{task_title}"\n')
